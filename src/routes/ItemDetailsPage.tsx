0990d749d583e93fdd3f1b232fed3993746a7076
Sumedha Pramod <sumedha.pramod@gmail.com>
2020-03-31 11:10:08 -0400

    Refactoring props

--- src/routes/ItemDetailsPage.tsx
+++ src/routes/ItemDetailsPage.tsx
@@ -1,69 +1,35 @@
 import React from "react";
 import { RouteComponentProps } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
-import {
-  TextField,
-  Box,
-  Typography,
-  Button,
-} from "@material-ui/core";
-import Rating from '@material-ui/lab/Rating';
+import { TextField, Box, Typography, Button } from "@material-ui/core";
+import Rating from "@material-ui/lab/Rating";
 
-import * as firebase from 'firebase/app';
+import * as firebase from "firebase/app";
 
 import ReviewsList from "../components/ReviewsList";
 import Layout from "../components/Layout/Layout";
 import { TACO_API_BASE, RouteParams } from "../utils/globals";
 
-type ItemDetails = {
-  name: string;
-  slug: string;
-  recipe: string;
-  url: string;
-};
-
-export type Review = {
-  recipeName: string;
-  commenterName: string;
-  text: string;
-  stars: number;
-}
-
 const ItemDetailsPage: React.FC<RouteComponentProps> = ({ match }) => {
   const { slug } = match.params as RouteParams;
-  const [ItemDetails, setItemDetails] = React.useState<ItemDetails>();
+  const [itemDetails, setItemDetails] = React.useState<ItemDetails>();
   const [reviews, setReviews] = React.useState<Array<Review>>([]);
-  const reviewsTable: firebase.database.Reference = firebase.database().ref('reviews');
+  const reviewsTable: firebase.database.Reference = firebase
+    .database()
+    .ref("reviews");
 
-  const loadReviews = async (recipeName: string): Promise<void> => {
-    let reviews: Array<Review> = [];
-    await reviewsTable
-      .orderByChild('recipe_name')
-      .equalTo(recipeName)
-      .once("value")
-      .then((data) => {
-        data.forEach((childNode) => {
-          const row = childNode.val();
-          const review: Review = {
-            recipeName: row['recipe_name'],
-            commenterName: row['commenter_name'],
-            text: row['text'],
-            stars: row['stars'],
-          };
-          reviews.unshift(review);
-        });
-      });
-    setReviews(reviews);
-  }
-
-  const saveReview = async (commenterName: string, stars: number, review: string): Promise<void> => {
+  const saveReview = async (
+    commenterName: string,
+    stars: number,
+    review: string
+  ): Promise<void> => {
     await reviewsTable.push({
       commenter_name: commenterName,
       stars: stars,
       text: review,
-      recipe_name: ItemDetails?.name,
+      recipe_name: itemDetails?.name
     });
-  }
+  };
 
   React.useEffect(() => {
     fetch(`${TACO_API_BASE}/toppings/${slug}.json`)
@@ -72,8 +38,28 @@ const ItemDetailsPage: React.FC<RouteComponentProps> = ({ match }) => {
   }, [slug]);
 
   React.useEffect(() => {
-    ItemDetails && loadReviews(ItemDetails.name);
-  }, [ItemDetails])
+    if (!itemDetails) return;
+
+    let reviews: Array<Review> = [];
+    reviewsTable
+      .orderByChild("recipe_name")
+      .equalTo(itemDetails.name)
+      .once("value")
+      .then(data => {
+        data.forEach(childNode => {
+          const row = childNode.val();
+          const review: Review = {
+            recipeName: row["recipe_name"],
+            commenterName: row["commenter_name"],
+            text: row["text"],
+            stars: row["stars"]
+          };
+          reviews.unshift(review);
+        });
+
+        setReviews(reviews);
+      });
+  }, [itemDetails, itemDetails?.name, saveReview]);
 
   const [review, updateReview] = React.useState<string>();
   const [name, updateName] = React.useState<string>();
@@ -81,13 +67,13 @@ const ItemDetailsPage: React.FC<RouteComponentProps> = ({ match }) => {
 
   return (
     <Layout>
-      {ItemDetails && (
+      {itemDetails && (
         <>
-          <h1>{ItemDetails.name}</h1>
-          <ReactMarkdown source={ItemDetails.recipe} />
+          <h1>{itemDetails.name}</h1>
+          <ReactMarkdown source={itemDetails.recipe} />
         </>
       )}
-      <Box style={{ paddingTop: "20px"}}>
+      <Box style={{ paddingTop: "20px" }}>
         <Typography variant="h6">Leave a review</Typography>
         <Box display="inline">
           <Rating
@@ -128,7 +114,6 @@ const ItemDetailsPage: React.FC<RouteComponentProps> = ({ match }) => {
                 updateReview("");
                 if (review) {
                   saveReview(name || "Anonymous", stars, review);
-                  ItemDetails && loadReviews(ItemDetails.name);
                 }
               }}
             >
@@ -136,9 +121,7 @@ const ItemDetailsPage: React.FC<RouteComponentProps> = ({ match }) => {
             </Button>
           </Box>
         </Box>
-        <ReviewsList
-          reviews={reviews}
-        />
+        <ReviewsList reviews={reviews} />
       </Box>
     </Layout>
   );
