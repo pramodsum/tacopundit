a0bb1998c42d6d5944cd19866a1c26827e02f190
Sumedha Pramod <sumedha.pramod@gmail.com>
2020-03-31 11:34:23 -0400

    Fix reviewItem css import

--- src/components/ReviewItem/ReviewItem.tsx
+++ src/components/ReviewItem/ReviewItem.tsx
@@ -3,7 +3,7 @@ import { TableRow, TableCell } from "@material-ui/core";
 import Rating from "@material-ui/lab/Rating";
 
 import { Review } from "../../utils/types";
-import "./Review.css";
+import "./ReviewItem.css";
 
 const ReviewItem: React.FC<Review> = review => (
   <TableRow>
